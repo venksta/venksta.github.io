2dcdd6a3069d4949ba4e28aebdf7fea05384ab89
Venk Seelam <venk2005@gmail.com>
2013-07-24 10:02:20 -0700

    Update application.js

--- 37Signals/application.js
+++ 37Signals/application.js
@@ -1,40 +1,47 @@
 $(document).ready(function(){
 
-// basecamp rollover
 
-	$(".bc").mouseenter(function(){
-		$(".headline").hide();
-		$(".headline-bc").show();
-		$(".bc .arrow").show();
- 	});
-	$(".bc").mouseleave(function(){
-		$(".headline").show();
-		$(".headline-bc").hide();
-		$(".bc .arrow").hide();
+	
+		$(".basecamp").on("mouseenter", function(){
+			event.preventDefault();
+			$(".headliner").hide();
+			$(this).find(".leftarrow").show();
+			$(".alt-basecamp").show();
 	});	
-// highrise button rollover
 
-	$(".hr").mouseenter(function(){
-		$(".headline").hide();
-		$(".headline-hr").show();
-		$(".hr .arrow").show();
- 	});
- 	$(".hr").mouseleave(function(){
-		$(".headline").show();
-		$(".headline-hr").hide();
-		$(".hr .arrow").hide();
- 	});	
+		$(".basecamp").on("mouseleave", function(){
+			event.preventDefault();
+			$(this).find(".leftarrow").hide();
+			$(".alt-basecamp").hide();
+			$(".headliner").show();
+	});	
 
-// campfire button rollover
+		$(".highrise").on("mouseenter", function(){
+			event.preventDefault();
+			$(".headliner").hide();
+			$(this).find(".leftarrow").show();
+			$(".alt-highrise").show();
+	});	
+	
+		$(".highrise").on("mouseleave", function(){
+			event.preventDefault();
+			$(this).find(".leftarrow").hide();
+			$(".alt-highrise").hide();
+			$(".headliner").show();
+	});	
 
-	$(".cf").mouseenter(function(){
-		$(".headline").hide();
-		$(".headline-cf").show();
-		$(".cf .arrow").show();
- 	});
- 	$(".cf").mouseleave(function(){
-		$(".headline").show();
-		$(".headline-cf").hide();
-		$(".cf .arrow").hide();
- 	});		
+		$(".campfire").on("mouseenter", function(){
+			event.preventDefault();
+			$(".headliner").hide();
+			$(this).find(".rightarrow").show();
+			$(".alt-campfire").show();
+	});	
+	
+		$(".campfire").on("mouseleave", function(){
+			event.preventDefault();
+			$(this).find(".rightarrow").hide();
+			$(".alt-campfire").hide();
+			$(".headliner").show();
+	});	
+
 });
