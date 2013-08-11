0c634e14a66dfa04853b3ccf99730cbada4c1a4c
Venk Seelam <venk2005@gmail.com>
2013-08-10 17:42:52 -0700

    chages

---
+++ hotcold/application.js
@@ -0,0 +1,40 @@
+$(document).ready function(){
+
+
+var answer = Math.ceil(Math.random() *100);
+
+//Guesses by the user:
+
+function getAnswer(computerAnswer, humanGuess){
+
+var difference=Math.abs(computerAnswer - humanGuess)
+
+if(humanGuess === computerAnswer){
+	return "Wow, you actually guessed right!";
+}
+
+else if(difference > 0 && difference <=10){
+	return "Hot damn!";
+}
+
+else if(difference >=11 && difference <= 20){
+	return "Warming up!";
+}
+else if(difference >=21 && difference <= 30){
+	return "A little chilly";
+}
+
+else if(difference >=31){
+	return "It's getting cold!";
+}
+
+else{
+	return "Not even close!";
+}
+
+}
+
+
+
+
+});
